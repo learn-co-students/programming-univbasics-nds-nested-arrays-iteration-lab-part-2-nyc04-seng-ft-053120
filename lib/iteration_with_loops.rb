3c38065d0c3e7b44a26f6df926d6eb02611e0a8b
Karem Ceron <karemiceron@gmail.com>
2020-06-04 14:44:48 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,24 +1,3 @@
-=begin
-require 'pry'
-def find_min_in_nested_arrays(src)
-  min_values_array = []
-  row_index = 0 
-  while row_index < src.count do 
-    element_index = 0 
-    min_values = 0  
-    while element_index < src[row_index].count do 
-      sorted_array = src[row_index][element_index].sort 
-      min_values_array << sorted_array[0]
-      binding.pry 
-      end 
-        element_index += 1
-      end
-      row_index += 1 
-      min_values_array
-end 
-find_min_in_nested_arrays(src)
-=end 
-  
 def find_min_in_nested_arrays(src)
   min_values_array = []
   row_index = 0 
